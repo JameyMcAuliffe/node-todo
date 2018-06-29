7b5670a78afb41de602216bc5777e54955909aae
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-06-29 15:25:19 -0500

    added patch route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -72,11 +73,40 @@ app.delete('/todos/:id', (req, res) => {
 		if(!todo) {
 			return res.status(404).send();
 		}
-		res.status(200).send(todo);
+		res.status(200).send({todo});
 	}).catch((e) => {
 		res.status(400).send();
 	});
 });
+
+//Update todo by ID
+app.patch('/todos/:id', (req, res) => {
+	let id = req.params.id;
+	//pick takes an object then an array of properties to pull off
+	let body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
