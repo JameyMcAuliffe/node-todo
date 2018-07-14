a807d546faafe91ebe9e7e0988e8fd7b4405cdb2
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-07-13 20:58:05 -0500

    added login route

--- server/models/user.js
+++ server/models/user.js
@@ -80,6 +80,26 @@ UserSchema.statics.findByToken = function(token) {
 	});
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+	let User = this;
+
+	return User.findOne({email}).then((user) => {
+		if(!user) {
+			return Promise.reject();
+		}
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, res) => {
+				if(res) {
+					resolve(user);
+				} else {
+					reject();
+				}
+			});
+		});
+	});
+};
+
 //Middleware to run password check before saving a doc
 UserSchema.pre('save', function(next) {
 	let user = this;
